7dfb651b3a712844d7d56c78346b5b3b7cafc468
Vikk_Moor <victimofuroboros@gmail.com>
2022-11-29 00:47:58 +0500

    Last

---
+++ 5th/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 45: Напишите программу, 
+// которая будет создавать копию заданного массива с помощью поэлементного копирования.
+void PrintArray(int[] arrN)
+{
+    int len = arrN.Length;
+    int pos = 0;
+    while (pos < len)
+    {
+        Console.Write($"{arrN[pos]} ");
+        pos++;
+    }
+}
+Console.Write("Enter array elements in order separated by commas or spaces: ");
+string s = Console.ReadLine();
+string[] nums = s.Split(new char[] { ' ', '.', ',' }, StringSplitOptions.RemoveEmptyEntries);
+int[] array = nums.Select(Int32.Parse).ToArray();
+int[] copyarr = new int[array.Length];
+for (int i = 0; i < copyarr.Length; i++) copyarr[i] = array[i];
+PrintArray(copyarr);
