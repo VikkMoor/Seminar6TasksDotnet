7f8d08de69057ecf8c62b82920a1af17c0fb0110
Victoria Moor <115675892+VikkMoor@users.noreply.github.com>
2022-11-30 23:05:59 +0500

    4th!

--- 4th/Program.cs
+++ 4th/Program.cs
@@ -1,7 +1,3 @@
-﻿// Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
-// Если N = 5 -> 0 1 1 2 3
-// Если N = 3 -> 0 1 1
-// Если N = 7 -> 0 1 1 2 3 5 8
 void PrintArray(int[] arr)
 {
     int len = arr.Length;
@@ -20,5 +16,3 @@ arrr[0] = 0;
 arrr[1] = arrr[2] = 1;
 for (int i =3; i < arrr.Length; i++) arrr[i] = arrr[i - 1] + arrr[i - 2];
 PrintArray(arrr);
-
-
